1775cd10542d108fa49701c108bb500e41e1630f
Natalie-Cardot <nataliecardot@gmail.com>
2019-08-25 15:48:40 -0700

    Add attack method

--- app.js
+++ app.js
@@ -10,6 +10,26 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100,
       this.demonHealth = 100
+    },
+    attack: function() {
+      let max = 10;
+      let min = 3;
+      let damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.demonHealth -= damage;
+
+      max = 12;
+      min = 5;
+      damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.playerHealth -= damage;
+    },
+    specialAttack: function() {
+
+    },
+    heal: function() {
+
+    },
+    giveUp: function() {
+
     }
   }
 });

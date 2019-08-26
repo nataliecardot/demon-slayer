b346f59650d47b4e540d0a87448fecaed3187d91
Natalie-Cardot <nataliecardot@gmail.com>
2019-08-26 14:20:58 -0700

    Implement giveUp method

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ new Vue({
       this.demonAttack();
     },
     giveUp: function() {
-
+      this.gameIsRunning = false;
     },
     demonAttack: function() {
       this.playerHealth -= this.calculateDamage(5, 12);
